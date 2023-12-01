0ccd0f99ee299383085dd417f30df437cfb6a8f0
Benjamin Peralta <benjaminp158@gmail.com>
2023-12-01 14:54:38 -0300

    Feat: Adding functional create task button

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let addTaskButtons = [...document.getElementsByClassName('add-task')]
+
+let todaySection = document.getElementById('TodaySection')
+
+
+addTaskButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        let textInput = document.createElement('input')
+        textInput.setAttribute('type', 'text')
+        let confirmButton = document.createElement('button')
+        confirmButton.innerHTML = 'Create Task'
+        button.insertAdjacentElement('afterend', textInput)
+        textInput.insertAdjacentElement('afterend', confirmButton)
+        
+        confirmButton.onclick = function(){
+            createTask(textInput.value)
+            todaySection.removeChild(textInput)
+            todaySection.removeChild(confirmButton)
+
+        }
+    })
+})
+
+
+function createTask(taskName){
+    let task = document.createElement('p')
+    task.innerHTML = taskName
+    todaySection.appendChild(task)
+}
+alert("Listo rey")
